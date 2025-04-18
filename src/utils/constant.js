254c51d969f11d96442a8f488b1b82e09e0b72f2
Ayush Ninawe <66631743+AyushNinawe@users.noreply.github.com>
2025-04-18 18:21:54 +0530

    link

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,11 @@
-export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
-export const TWEET_API_END_POINT="http://localhost:8080/api/v1/tweet";
+// export const USER_API_END_POINT="http://localhost:8080/api/v1/user";
+// export const TWEET_API_END_POINT="http://localhost:8080/api/v1/tweet";
+export const USER_API_END_POINT="https://final-twitter-backend.onrender.com/api/v1/user"
+export const TWEET_API_END_POINT="https://final-twitter-backend.onrender.com/api/v1/tweet"
+
 
 // export const timeSince =(timestamp)=>{
-//     let time = Date.parse(timestamp);
+//     let time = Date.parse(timestamp);https://final-twitter-backend.onrender.com
 //     let now = Date.now();
 //     let secondsPast = (now - time) /1000;
 //     let suffix ='ago';
